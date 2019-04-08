dade0250108e2661963178e857c40af00d5b5f05
Damian <damian.straszak@gmail.com>
2019-04-08 10:59:26 +0200

    Added utils with min and max functions.

---
+++ pkg/utils/math.go
@@ -0,0 +1,17 @@
+package utils
+
+func Min(x, y int) int {
+	if x <= y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func Max(x, y int) int {
+	if x >= y {
+		return x
+	} else {
+		return y
+	}
+}
