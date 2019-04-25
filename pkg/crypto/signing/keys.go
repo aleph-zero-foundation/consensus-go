dfe71ac8e810fa41da0a8d81dc7dfd30b29c3c9f
Michal Handzlik <michal.handzlik@alephzero.org>
2019-04-25 16:13:47 +0200

    More comments in crypto/signing

--- pkg/crypto/signing/keys.go
+++ pkg/crypto/signing/keys.go
@@ -2,12 +2,14 @@ package signing
 
 import gomel "gitlab.com/alephledger/consensus-go/pkg"
 
-// PublicKey used for signature checking
+// PublicKey used for signature checking.
 type PublicKey interface {
+	// Verify checks if a preunit has a correct signature.
 	Verify(gomel.Preunit) bool
 }
 
-// PrivateKey used for signing units
+// PrivateKey used for signing units.
 type PrivateKey interface {
+	// Sign computes and returns a signature of a preunit.
 	Sign(gomel.Preunit) gomel.Signature
 }
