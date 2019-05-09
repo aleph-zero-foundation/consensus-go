ab36560ff04a30922f35d403fb7b77dd13e66fcb
Łukasz Lachowski <lukasz.lachowski@alephzero.org>
2019-05-09 12:18:39 +0200

    HOTFIX fixed make_coverage.sh - it was not outputting one of its reports.

--- .gitlab/ci/make_coverage.sh
+++ .gitlab/ci/make_coverage.sh
@@ -27,6 +27,6 @@ tail -q -n +2 "${COVERAGE_DIR}"/*.cov >> "${COVERAGE_DIR}/coverage.cov" ;
 go tool cover -func="${COVERAGE_DIR}/coverage.cov" -o ${REPORT_OUTPUT} ;
 
 # If needed, generate HTML report
-if [ -z "$3" ]; then
+if [ ! -z "$3" ]; then
     go tool cover -html="${COVERAGE_DIR}/coverage.cov" -o ${HTML_REPORT_OUTPUT} ;
 fi
