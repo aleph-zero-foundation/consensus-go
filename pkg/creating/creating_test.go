fa06e52ae4709b6d79125d9b6388c07813aec587
fruwajacybyk <maciej.gawron@alephzero.co>
2019-05-13 11:46:30 +0200

    Change creating package to use tests package for testing

--- pkg/creating/creating_test.go
+++ pkg/creating/creating_test.go
@@ -6,273 +6,82 @@ import (
 
 	gomel "gitlab.com/alephledger/consensus-go/pkg"
 	. "gitlab.com/alephledger/consensus-go/pkg/creating"
+	tests "gitlab.com/alephledger/consensus-go/pkg/tests"
 )
 
-type poset struct {
-	nProcesses   int
-	primeUnits   []gomel.SlottedUnits
-	maximalUnits gomel.SlottedUnits
-}
-
-func (p *poset) AddUnit(_ gomel.Preunit, _ func(gomel.Preunit, gomel.Unit, error)) {}
-
-func (p *poset) PrimeUnits(level int) gomel.SlottedUnits {
-	return p.primeUnits[level]
-}
-
-func (p *poset) MaximalUnitsPerProcess() gomel.SlottedUnits {
-	return p.maximalUnits
-}
-
-func (p *poset) IsQuorum(_ int) bool {
-	return false
-}
-
-func (p *poset) GetNProcesses() int {
-	return p.nProcesses
-}
-
-type slottedUnits struct {
-	contents [][]gomel.Unit
-}
-
-func (su *slottedUnits) Get(id int) []gomel.Unit {
-	return su.contents[id]
-}
-
-func (su *slottedUnits) Set(id int, units []gomel.Unit) {
-	su.contents[id] = units
-}
-
-func (su *slottedUnits) Iterate(work func([]gomel.Unit) bool) {
-	for _, units := range su.contents {
-		if !work(units) {
-			return
-		}
-	}
-}
-
-func newSlottedUnits(n int) gomel.SlottedUnits {
-	return &slottedUnits{
-		contents: make([][]gomel.Unit, n),
-	}
-}
-
-type unit struct {
-	creator            int
-	signature          gomel.Signature
-	hash               gomel.Hash
-	height             int
-	parents            []gomel.Unit
-	level              int
-	hasForkingEvidence map[int]bool
-}
-
-func (u *unit) Below(v gomel.Unit) bool {
-	toVisit := []gomel.Unit{v}
-	visiting := map[gomel.Hash]bool{}
-	visiting[*v.Hash()] = true
-	for len(toVisit) > 0 {
-		w := toVisit[0]
-		toVisit = toVisit[1:]
-		if w == u {
-			return true
-		}
-		for _, p := range w.Parents() {
-			if !visiting[*p.Hash()] {
-				toVisit = append(toVisit, p)
-				visiting[*p.Hash()] = true
-			}
-		}
-	}
-	return false
-}
-
-func (u *unit) Above(v gomel.Unit) bool {
-	return v.Below(u)
-}
-
-func (u *unit) Creator() int {
-	return u.creator
-}
-
-func (u *unit) Signature() gomel.Signature {
-	return u.signature
-}
-
-func (u *unit) Hash() *gomel.Hash {
-	return &u.hash
-}
-
-func (u *unit) Height() int {
-	return u.height
-}
-
-func (u *unit) Parents() []gomel.Unit {
-	return u.parents
-}
-
-func (u *unit) Level() int {
-	return u.level
-}
-
-func (u *unit) HasForkingEvidence(creator int) bool {
-	return u.hasForkingEvidence[creator]
-}
-
 var _ = Describe("Creating", func() {
-
-	var (
-		pu         []gomel.SlottedUnits
-		mu         gomel.SlottedUnits
-		p          *poset
-		nProcesses int
-	)
-
-	BeforeEach(func() {
-		mu = nil
-		pu = nil
-		nProcesses = 4
-	})
-
-	JustBeforeEach(func() {
-		p = &poset{
-			nProcesses:   nProcesses,
-			primeUnits:   pu,
-			maximalUnits: mu,
-		}
-	})
-
 	Describe("in a small poset", func() {
-
 		var (
-			maxUnitsInPoset   []gomel.Unit
-			primeUnitsInPoset []gomel.Unit
+			p  gomel.Poset
+			h1 gomel.Hash
+			h2 gomel.Hash
 		)
-
-		BeforeEach(func() {
-			pu = []gomel.SlottedUnits{}
-			for i := 0; i < 10; i++ {
-				pu = append(pu, newSlottedUnits(nProcesses))
-			}
-			mu = newSlottedUnits(nProcesses)
-			maxUnitsInPoset = nil
-			primeUnitsInPoset = nil
-		})
-
-		JustBeforeEach(func() {
-			for _, u := range maxUnitsInPoset {
-				id := u.Creator()
-				mu.Set(id, append(mu.Get(id), u))
-			}
-			for _, u := range primeUnitsInPoset {
-				id := u.Creator()
-				level := u.Level()
-				pu[level].Set(id, append(pu[level].Get(id), u))
-			}
-		})
-
 		Context("that is empty", func() {
-
+			BeforeEach(func() {
+				p, _ = tests.CreatePosetFromTestFile("../testdata/empty.txt", tests.NewTestPosetFactory())
+			})
 			It("should return a dealing unit", func() {
-				pu, err := NewUnit(p, 0, nProcesses)
+				pu, err := NewUnit(p, 0, p.GetNProcesses())
 				Expect(err).NotTo(HaveOccurred())
 				Expect(pu.Creator()).To(Equal(0))
 				Expect(pu.Parents()).To(BeEmpty())
 			})
-
 			It("should return a dealing unit", func() {
-				pu, err := NewUnit(p, 3, nProcesses)
+				pu, err := NewUnit(p, 3, p.GetNProcesses())
 				Expect(err).NotTo(HaveOccurred())
 				Expect(pu.Creator()).To(Equal(3))
 				Expect(pu.Parents()).To(BeEmpty())
 			})
-
 		})
 
 		Context("that contains a single dealing unit", func() {
-
 			BeforeEach(func() {
-				singleUnit := &unit{
-					creator: 0,
-					height:  0,
-					parents: nil,
-					level:   0,
-				}
-				singleUnit.hash[0] = 1
-				primeUnitsInPoset = append(primeUnitsInPoset, singleUnit)
-				maxUnitsInPoset = append(maxUnitsInPoset, singleUnit)
+				p, _ = tests.CreatePosetFromTestFile("../testdata/one_unit.txt", tests.NewTestPosetFactory())
 			})
-
 			It("should return a dealing unit for a different creator", func() {
-				pu, err := NewUnit(p, 3, nProcesses)
+				pu, err := NewUnit(p, 3, p.GetNProcesses())
 				Expect(err).NotTo(HaveOccurred())
 				Expect(pu.Creator()).To(Equal(3))
 				Expect(pu.Parents()).To(BeEmpty())
 			})
 
 			It("should fail due to not enough parents for the same creator", func() {
-				_, err := NewUnit(p, 0, nProcesses)
+				_, err := NewUnit(p, 0, p.GetNProcesses())
 				Expect(err).To(MatchError("No legal parents for the unit."))
 			})
-
 		})
 
 		Context("that contains two dealing units", func() {
-
 			BeforeEach(func() {
-				for id := 0; id < 2; id++ {
-					someUnit := &unit{
-						creator: id,
-						height:  0,
-						parents: nil,
-						level:   0,
-					}
-					someUnit.hash[0] = byte(id + 1)
-					primeUnitsInPoset = append(primeUnitsInPoset, someUnit)
-					maxUnitsInPoset = append(maxUnitsInPoset, someUnit)
-				}
+				p, _ = tests.CreatePosetFromTestFile("../testdata/two_dealing.txt", tests.NewTestPosetFactory())
+				h1 = *p.PrimeUnits(0).Get(0)[0].Hash()
+				h2 = *p.PrimeUnits(0).Get(1)[0].Hash()
 			})
-
 			It("should return a unit with these parents", func() {
-				pu, err := NewUnit(p, 0, nProcesses)
+				pu, err := NewUnit(p, 0, p.GetNProcesses())
 				Expect(err).NotTo(HaveOccurred())
 				Expect(pu.Creator()).To(Equal(0))
 				Expect(pu.Parents()).NotTo(BeEmpty())
 				Expect(len(pu.Parents())).To(BeEquivalentTo(2))
-				Expect(pu.Parents()[0][0]).To(BeEquivalentTo(1))
-				Expect(pu.Parents()[1][0]).To(BeEquivalentTo(2))
+				Expect(pu.Parents()[0]).To(BeEquivalentTo(h1))
+				Expect(pu.Parents()[1]).To(BeEquivalentTo(h2))
 			})
-
 		})
 
 		Context("that contains all the dealing units", func() {
-
 			BeforeEach(func() {
-				for id := 0; id < nProcesses; id++ {
-					someUnit := &unit{
-						creator: id,
-						height:  0,
-						parents: nil,
-						level:   0,
-					}
-					someUnit.hash[0] = byte(id + 1)
-					primeUnitsInPoset = append(primeUnitsInPoset, someUnit)
-					maxUnitsInPoset = append(maxUnitsInPoset, someUnit)
-				}
+				p, _ = tests.CreatePosetFromTestFile("../testdata/only_dealing.txt", tests.NewTestPosetFactory())
+				h1 = *p.PrimeUnits(0).Get(0)[0].Hash()
 			})
-
 			It("should return a unit with some parents", func() {
-				pu, err := NewUnit(p, 0, nProcesses)
+				pu, err := NewUnit(p, 0, p.GetNProcesses())
 				Expect(err).NotTo(HaveOccurred())
 				Expect(pu.Creator()).To(Equal(0))
 				Expect(pu.Parents()).NotTo(BeEmpty())
 				Expect(len(pu.Parents()) > 1).To(BeTrue())
-				Expect(pu.Parents()[0][0]).To(BeEquivalentTo(1))
+				Expect(pu.Parents()[0]).To(BeEquivalentTo(h1))
 			})
-
 		})
-
 	})
 
 })
--- pkg/tests/generator.go
+++ pkg/tests/generator.go
@@ -16,6 +16,11 @@ func CreateRandomNonForking(nProcesses, minParents, maxParents, nUnits int) gome
 	p := newPoset(nProcesses)
 	created := 0
 	for created < nUnits {
+		// TODO:
+		// (1) check if the compliance rules are satisfied
+		//		 (currently for this non-forking example expand prime rule might not be satisfied)
+		// (2) add new parameter to the function containing
+		//		 set of compliance rules which should be satisfied
 		pid := r.Intn(nProcesses)
 		if p.maximalHeight[pid] == -1 {
 			pu := newPreunit(pid, []gomel.Hash{})
