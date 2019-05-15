5f5695be758f5d156cdf55f0a2a405c08b0f7fad
fruwajacybyk <maciej.gawron@alephzero.co>
2019-05-15 17:00:43 +0200

    Refactoring to comply with the new service framework

--- pkg/linear/common_random_permutation.go
+++ pkg/linear/common_random_permutation.go
@@ -1,6 +1,28 @@
 package linear
 
+import (
+	gomel "gitlab.com/alephledger/consensus-go/pkg"
+)
+
 // CommonRandomPermutation represents random permutation shared between processes.
 type CommonRandomPermutation interface {
 	Get(level int) []int
+}
+
+type commonRandomPermutation struct {
+	n int
+}
+
+func (crp *commonRandomPermutation) Get(level int) []int {
+	permutation := make([]int, crp.n, crp.n)
+	for i := 0; i < crp.n; i++ {
+		permutation[i] = (i + level) % crp.n
+	}
+	return permutation
+}
+
+func NewCommonRandomPermutation(p gomel.Poset) CommonRandomPermutation {
+	return &commonRandomPermutation{
+		n: p.NProc(),
+	}
 }
