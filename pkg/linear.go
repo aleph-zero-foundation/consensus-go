6833b1fa2670d25c17f2f469ac665cca65accd94
Maciej Gawron <maciej.gawron@alephzero.co>
2019-05-23 16:07:11 +0000

    Hotfix: fixing outdated comment

--- pkg/linear.go
+++ pkg/linear.go
@@ -2,8 +2,8 @@ package gomel
 
 // LinearOrdering is an interface for establishing a linear order of units.
 type LinearOrdering interface {
-	// TimingRound returns all units in timing round r, that is all units that are below r-th timing unit but not below previous timing units.
-	// If any of those timing units has not yet been chosen, an error is returned.
+	// TimingRound establishes the linear ordering on the units in timing round r and returns them.
+	// If the timing decision has not yet been taken it returns nil.
 	TimingRound(int) []Unit
 	// DecideTimingOnLevel tries to pick a timing unit on a given level. Returns nil if it cannot be decided yet.
 	DecideTimingOnLevel(int) Unit
