54a56c5677c13952a98c1c543e76ad4a6a46df44
fruwajacybyk <maciej.gawron@alephzero.co>
2019-05-28 16:04:39 +0200

    Revert "Removed global threshold coin"
    This reverts commit b72b04fb2c667096e018284dd060791fe5b47929.

--- pkg/crypto/tcoin/tcoin_test.go
+++ pkg/crypto/tcoin/tcoin_test.go
@@ -10,33 +10,37 @@ import (
 var _ = Describe("Tcoin", func() {
 	var n, t int
 	var nonce int
-	var tc *ThresholdCoin
+	var gtc *GlobalThresholdCoin
+	var tc []*ThresholdCoin
 	var coinShares []*CoinShare
 	Context("Between small number of processes", func() {
 		BeforeEach(func() {
 			n, t = 10, 3
-			tc = GenerateThresholdCoin(n, t)
+			gtc = GenerateThresholdCoin(n, t)
+			for i := 0; i < n; i++ {
+				tc = append(tc, NewThresholdCoin(gtc, i))
+			}
 		})
 		Describe("Coin shares", func() {
 			BeforeEach(func() {
 				nonce = 123
 				coinShares = make([]*CoinShare, n)
-				for pid := 0; pid < n; pid++ {
-					coinShares[pid] = tc.CreateCoinShare(pid, nonce)
+				for i := 0; i < n; i++ {
+					coinShares[i] = tc[i].CreateCoinShare(nonce)
 				}
 			})
 			It("should be verified correctly", func() {
-				Expect(tc.VerifyCoinShare(coinShares[1], nonce)).To(BeTrue())
-				Expect(tc.VerifyCoinShare(coinShares[1], nonce+1)).To(BeFalse())
+				Expect(tc[1].VerifyCoinShare(coinShares[2], nonce)).To(BeTrue())
+				Expect(tc[1].VerifyCoinShare(coinShares[2], nonce+1)).To(BeFalse())
 			})
 			It("Should be correctly combined by t-parties", func() {
-				c, ok := tc.CombineCoinShares(coinShares[:t])
+				c, ok := tc[0].CombineCoinShares(coinShares[:t])
 				Expect(ok).To(BeTrue())
-				Expect(tc.VerifyCoin(c, nonce)).To(BeTrue())
-				Expect(tc.VerifyCoin(c, nonce+1)).To(BeFalse())
+				Expect(tc[0].VerifyCoin(c, nonce)).To(BeTrue())
+				Expect(tc[0].VerifyCoin(c, nonce+1)).To(BeFalse())
 			})
 			It("Shouldn't be correctly combined by t-1-parties", func() {
-				_, ok := tc.CombineCoinShares(coinShares[:(t - 1)])
+				_, ok := tc[0].CombineCoinShares(coinShares[:(t - 1)])
 				Expect(ok).To(BeFalse())
 			})
 		})
