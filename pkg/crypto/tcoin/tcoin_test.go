0f0bda763dd977f7c328088eee8d06ce2dfa6492
fruwajacybyk <maciej.gawron@alephzero.co>
2019-05-28 14:15:31 +0200

    Removed global threshold coin

--- pkg/crypto/tcoin/tcoin_test.go
+++ pkg/crypto/tcoin/tcoin_test.go
@@ -10,37 +10,33 @@ import (
 var _ = Describe("Tcoin", func() {
 	var n, t int
 	var nonce int
-	var gtc *GlobalThresholdCoin
-	var tc []*ThresholdCoin
+	var tc *ThresholdCoin
 	var coinShares []*CoinShare
 	Context("Between small number of processes", func() {
 		BeforeEach(func() {
 			n, t = 10, 3
-			gtc = GenerateThresholdCoin(n, t)
-			for i := 0; i < n; i++ {
-				tc = append(tc, NewThresholdCoin(gtc, i))
-			}
+			tc = GenerateThresholdCoin(n, t)
 		})
 		Describe("Coin shares", func() {
 			BeforeEach(func() {
 				nonce = 123
 				coinShares = make([]*CoinShare, n)
-				for i := 0; i < n; i++ {
-					coinShares[i] = tc[i].CreateCoinShare(nonce)
+				for pid := 0; pid < n; pid++ {
+					coinShares[pid] = tc.CreateCoinShare(pid, nonce)
 				}
 			})
 			It("should be verified correctly", func() {
-				Expect(tc[1].VerifyCoinShare(coinShares[2], nonce)).To(BeTrue())
-				Expect(tc[1].VerifyCoinShare(coinShares[2], nonce+1)).To(BeFalse())
+				Expect(tc.VerifyCoinShare(coinShares[1], nonce)).To(BeTrue())
+				Expect(tc.VerifyCoinShare(coinShares[1], nonce+1)).To(BeFalse())
 			})
 			It("Should be correctly combined by t-parties", func() {
-				c, ok := tc[0].CombineCoinShares(coinShares[:t])
+				c, ok := tc.CombineCoinShares(coinShares[:t])
 				Expect(ok).To(BeTrue())
-				Expect(tc[0].VerifyCoin(c, nonce)).To(BeTrue())
-				Expect(tc[0].VerifyCoin(c, nonce+1)).To(BeFalse())
+				Expect(tc.VerifyCoin(c, nonce)).To(BeTrue())
+				Expect(tc.VerifyCoin(c, nonce+1)).To(BeFalse())
 			})
 			It("Shouldn't be correctly combined by t-1-parties", func() {
-				_, ok := tc[0].CombineCoinShares(coinShares[:(t - 1)])
+				_, ok := tc.CombineCoinShares(coinShares[:(t - 1)])
 				Expect(ok).To(BeFalse())
 			})
 		})
