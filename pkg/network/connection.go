3a582379828d8625c42a54c6f13949adef14f6e9
Michal Handzlik <michal.handzlik@alephzero.org>
2019-05-30 16:18:59 +0200

    Add some comments

--- pkg/network/connection.go
+++ pkg/network/connection.go
@@ -8,6 +8,8 @@ type Connection interface {
 	Write([]byte) (int, error)
 	Close() error
 	TimeoutAfter(t time.Duration)
+	// PID of the committee member on the other side of the connection.
 	Pid() uint16
+	// Sync ID, serial number counted for each PID separately.
 	Sid() uint32
 }
