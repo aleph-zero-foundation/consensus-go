a8e084fa6eeb6e98e2999ea4ea3a10d09658698e
Michal Handzlik <michal.handzlik@alephzero.org>
2019-05-30 15:46:10 +0200

    Better error messages in log4humanz

--- cmd/log4humanz/main.go
+++ cmd/log4humanz/main.go
@@ -12,16 +12,26 @@ import (
 func main() {
 	flag.Parse()
 	if flag.NArg() != 1 {
-		fmt.Fprintln(os.Stderr, "log4humanz: I need a file with JSON log as a single argument")
+		fmt.Fprintln(os.Stderr, "Usage: log4humanz logfile.json")
 		return
 	}
 
-	file, err := os.Open(flag.Args()[0])
-	if err != nil {
+	var file *os.File
+	switch _, err := os.Stat(flag.Args()[0]); {
+	case os.IsNotExist(err):
 		fmt.Fprintf(os.Stderr, "%s: file not present\n", flag.Args()[0])
 		return
+	case err != nil:
+		fmt.Fprintf(os.Stderr, "%s: cannot open file\n", flag.Args()[0])
+		return
+	default:
+		file, err = os.Open(flag.Args()[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: cannot open file\n", flag.Args()[0])
+			return
+		}
+		defer file.Close()
 	}
-	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	decoder := logging.NewDecoder(os.Stdout)
