91ab398eed54ff6fb9cecf48f8254adb47872a0d
Łukasz Lachowski <lukasz.lachowski@alephzero.org>
2019-06-03 17:21:09 +0200

    Added initial version of offline-poset byzantine test. It consists of three simple strategies: fork at some level and continue, create some amount of prime units and continue, and randomly forking strategy.

---
+++ tests/offline_poset/byzantine_tests/byzantine_linear_ordering_test.go
@@ -0,0 +1,264 @@
+package byzantine_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+
+	"golang.org/x/crypto/sha3"
+
+	gomel "gitlab.com/alephledger/consensus-go/pkg"
+	"gitlab.com/alephledger/consensus-go/pkg/creating"
+	"gitlab.com/alephledger/consensus-go/pkg/growing"
+	"gitlab.com/alephledger/consensus-go/tests/offline_poset"
+)
+
+const (
+	forkingPrimes = int(1000)
+	floodingLevel = 10
+)
+
+// Implementation of the Preunit interface that allows to create arbitrary forks without changing units content
+type preunitWithNonce struct {
+	gomel.Preunit
+	nounce uint64
+	hash   gomel.Hash
+}
+
+func toBytes(data interface{}) []byte {
+	var newData bytes.Buffer
+	binary.Write(&newData, binary.LittleEndian, data)
+	return newData.Bytes()
+}
+
+func (pu *preunitWithNonce) computeHash() {
+	hash := *pu.Preunit.Hash()
+
+	var data bytes.Buffer
+	data.Write(hash[:])
+	data.Write(toBytes(pu.nounce))
+	sha3.ShakeSum256(pu.hash[:len(pu.hash)], data.Bytes())
+}
+
+func NewPreunitWithNounce(preunit gomel.Preunit, nounce uint64) *preunitWithNonce {
+	pu := creating.NewPreunit(preunit.Creator(), preunit.Parents(), nil)
+
+	result := preunitWithNonce{Preunit: pu, nounce: nounce}
+	result.computeHash()
+	return &result
+}
+
+func (pu *preunitWithNonce) Hash() *gomel.Hash {
+	return &pu.hash
+}
+
+func createForks(preunit gomel.Preunit, privKey gomel.PrivateKey, count int) []gomel.Preunit {
+	result := make([]gomel.Preunit, 0, count)
+	created := map[gomel.Hash]bool{}
+	for i := uint64(0); len(result) < count; i++ {
+		fork := NewPreunitWithNounce(preunit, i)
+		if created[*fork.Hash()] {
+			continue
+		}
+		fork.SetSignature(privKey.Sign(fork))
+		result = append(result, fork)
+		created[*fork.Hash()] = true
+	}
+	return result
+}
+
+func computeMaxPossibleNumberOfByzantineProcesses(nProc int) int {
+	max := nProc / 3
+	if 3*max < nProc {
+		return max
+	} else {
+		return max - 1
+	}
+}
+
+func getRandomListOfByzantinePosets(n int) []int {
+	byzProcesses := computeMaxPossibleNumberOfByzantineProcesses(n)
+	return rand.Perm(byzProcesses)[:byzProcesses]
+}
+
+func newTriggeredAdder(triggerCondition func(unit gomel.Unit) bool, wrappedHandler offline_poset.AddingHandler) offline_poset.AddingHandler {
+
+	return func(posets []*growing.Poset, unit gomel.Preunit) error {
+		newUnit, err := offline_poset.AddToPosets(unit, posets)
+		if err != nil {
+			return err
+		}
+		if triggerCondition(newUnit) {
+			return wrappedHandler(posets, unit)
+		}
+		return nil
+
+	}
+}
+
+func newSimpleForkingAdder(forkingLevel int, privKeys []gomel.PrivateKey, byzantinePosets []int) offline_poset.AddingHandler {
+	alreadyForked := make(map[int]bool, len(byzantinePosets))
+	for _, posetID := range byzantinePosets {
+		alreadyForked[posetID] = false
+	}
+
+	return newTriggeredAdder(
+		func(unit gomel.Unit) bool {
+			val, ok := alreadyForked[unit.Creator()]
+			if ok && !val && unit.Level() >= forkingLevel {
+				return true
+			}
+			return false
+		},
+
+		func(posets []*growing.Poset, unit gomel.Preunit) error {
+			fmt.Println("simple forking behavior triggered")
+			units := createForks(unit, privKeys[unit.Creator()], 2)
+			err := offline_poset.AddUnitsToPosetsInRandomOrder(units, posets)
+			if err != nil {
+				return err
+			}
+			alreadyForked[unit.Creator()] = true
+			fmt.Println("simple fork created at level", forkingLevel)
+			return nil
+		},
+	)
+}
+
+func newPrimeFloodAdder(floodingLevel int, numberOfPrimes int, privKeys []gomel.PrivateKey, byzantinePosets []int) offline_poset.AddingHandler {
+	alreadyFlooded := make(map[int]bool, len(byzantinePosets))
+	for _, posetID := range byzantinePosets {
+		alreadyFlooded[posetID] = false
+	}
+
+	return newTriggeredAdder(
+		func(unit gomel.Unit) bool {
+			val, ok := alreadyFlooded[unit.Creator()]
+			if ok && !val && unit.Level() >= floodingLevel && gomel.Prime(unit) {
+				return true
+			}
+			return false
+		},
+
+		func(posets []*growing.Poset, unit gomel.Preunit) error {
+			fmt.Println("Prime flooding started")
+			for _, unit := range createForks(unit, privKeys[unit.Creator()], numberOfPrimes) {
+				if _, err := offline_poset.AddToPosets(unit, posets); err != nil {
+					return err
+				}
+			}
+			alreadyFlooded[unit.Creator()] = true
+			fmt.Println("Prime flooding finished")
+			return nil
+		},
+	)
+}
+
+func newRandomForkingAdder(byzantinePosets []int, forkProbability int, privKeys []gomel.PrivateKey) offline_poset.AddingHandler {
+	forkers := make(map[int]bool, len(byzantinePosets))
+	for _, creator := range byzantinePosets {
+		forkers[creator] = true
+	}
+
+	random := rand.New(rand.NewSource(0))
+
+	return newTriggeredAdder(
+		func(unit gomel.Unit) bool {
+			if forkers[unit.Creator()] && random.Intn(100) <= forkProbability {
+				return true
+			}
+			return false
+		},
+
+		func(posets []*growing.Poset, unit gomel.Preunit) error {
+			fmt.Println("random forking")
+			const forkSize = 2
+			for _, unit := range createForks(unit, privKeys[unit.Creator()], forkSize) {
+				if _, err := offline_poset.AddToPosets(unit, posets); err != nil {
+					return err
+				}
+			}
+			fmt.Println("random forking finished")
+			return nil
+		},
+	)
+}
+
+func testPrimeFloodingScenario() error {
+	const (
+		nProcesses = 21
+		nUnits     = 1000
+		maxParents = 2
+	)
+
+	pubKeys, privKeys := offline_poset.GenerateKeys(nProcesses)
+
+	unitCreator := offline_poset.NewDefaultUnitCreator(privKeys)
+	byzantinePosets := getRandomListOfByzantinePosets(nProcesses)
+	unitAdder := newPrimeFloodAdder(floodingLevel, forkingPrimes, privKeys, byzantinePosets)
+	verifier := offline_poset.NewDefaultVerifier()
+
+	return offline_poset.Test(pubKeys, nUnits, maxParents, unitCreator, unitAdder, verifier)
+}
+
+func testSimpleScenario() error {
+	const (
+		nProcesses = 21
+		nUnits     = 1000
+		maxParents = 2
+	)
+
+	pubKeys, privKeys := offline_poset.GenerateKeys(nProcesses)
+
+	unitCreator := offline_poset.NewDefaultUnitCreator(privKeys)
+	byzantinePosets := getRandomListOfByzantinePosets(nProcesses)
+	unitAdder := newSimpleForkingAdder(10, privKeys, byzantinePosets)
+	verifier := offline_poset.NewDefaultVerifier()
+
+	return offline_poset.Test(pubKeys, nUnits, maxParents, unitCreator, unitAdder, verifier)
+}
+
+func testRandomForking() error {
+	const (
+		nProcesses = 21
+		nUnits     = 1000
+		maxParents = 2
+	)
+
+	pubKeys, privKeys := offline_poset.GenerateKeys(nProcesses)
+
+	unitCreator := offline_poset.NewDefaultUnitCreator(privKeys)
+	byzantinePosets := getRandomListOfByzantinePosets(nProcesses)
+	unitAdder := newRandomForkingAdder(byzantinePosets, 50, privKeys)
+	verifier := offline_poset.NewDefaultVerifier()
+
+	return offline_poset.Test(pubKeys, nUnits, maxParents, unitCreator, unitAdder, verifier)
+}
+
+var _ = Describe("Byzantine Poset Test", func() {
+	Describe("simple scenario", func() {
+		It("should finish without errors", func() {
+			err := testSimpleScenario()
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Describe("prime flooding scenario", func() {
+		It("should finish without errors", func() {
+			err := testPrimeFloodingScenario()
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Describe("random forking scenario", func() {
+		It("should finish without errors", func() {
+			err := testRandomForking()
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+})
---
+++ tests/offline_poset/helpers.go
@@ -0,0 +1,315 @@
+package offline_poset
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sort"
+	"sync"
+
+	gomel "gitlab.com/alephledger/consensus-go/pkg"
+	"gitlab.com/alephledger/consensus-go/pkg/config"
+	"gitlab.com/alephledger/consensus-go/pkg/creating"
+	"gitlab.com/alephledger/consensus-go/pkg/crypto/signing"
+	"gitlab.com/alephledger/consensus-go/pkg/growing"
+	"gitlab.com/alephledger/consensus-go/pkg/linear"
+)
+
+const (
+	maxParents = 2
+)
+
+type UnitCreator func([]*growing.Poset) (gomel.Preunit, error)
+
+type AddingHandler func(posets []*growing.Poset, unit gomel.Preunit) error
+
+type PosetVerifier func([]*growing.Poset) error
+
+func NewDefaultAdder() AddingHandler {
+	return func(posets []*growing.Poset, unit gomel.Preunit) error {
+		_, err := AddToPosets(unit, posets)
+		return err
+	}
+}
+
+func AddToPoset(poset gomel.Poset, pu gomel.Preunit) (gomel.Unit, error) {
+	var result gomel.Unit
+	var caughtError error
+	var wg sync.WaitGroup
+	wg.Add(1)
+	poset.AddUnit(pu, func(pu gomel.Preunit, u gomel.Unit, err error) {
+		result = u
+		caughtError = err
+		wg.Done()
+	})
+	wg.Wait()
+	return result, caughtError
+}
+
+func AddToPosets(unit gomel.Preunit, posets []*growing.Poset) (resultUnit gomel.Unit, err error) {
+	for ix, poset := range posets {
+		result, errTmp := AddToPoset(poset, unit)
+		if errTmp != nil {
+			err = errTmp
+			return
+		}
+		if ix == unit.Creator() {
+			resultUnit = result
+		}
+	}
+	return
+}
+
+func AddUnitsToPosetsInRandomOrder(units []gomel.Preunit, posets []*growing.Poset) error {
+	for _, poset := range posets {
+		rand.Shuffle(len(units), func(i, j int) {
+			units[i], units[j] = units[j], units[i]
+		})
+
+		for _, pu := range units {
+			if _, err := AddToPoset(poset, pu); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func GenerateKeys(nProcesses int) (pubKeys []gomel.PublicKey, privKeys []gomel.PrivateKey) {
+	pubKeys = make([]gomel.PublicKey, 0, nProcesses)
+	privKeys = make([]gomel.PrivateKey, 0, nProcesses)
+	for i := 0; i < nProcesses; i++ {
+		pubKey, privKey, _ := signing.GenerateKeys()
+		pubKeys = append(pubKeys, pubKey)
+		privKeys = append(privKeys, privKey)
+	}
+	return pubKeys, privKeys
+}
+
+func NewDefaultUnitCreator(privKeys []gomel.PrivateKey) UnitCreator {
+	return func(posets []*growing.Poset) (gomel.Preunit, error) {
+		attempts := 0
+		for {
+			attempts++
+			if attempts%50 == 0 {
+				fmt.Println("Attempt no", attempts, "of creating a new unit")
+			}
+
+			creator := rand.Intn(len(posets))
+			poset := posets[creator]
+
+			pu, err := creating.NewUnit(poset, creator, maxParents, nil)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error while creating a new unit:", err)
+				continue
+			}
+			pu.SetSignature(privKeys[creator].Sign(pu))
+			return pu, nil
+		}
+	}
+}
+
+func GetOrderedUnits(poset gomel.Poset) (units chan gomel.Unit) {
+	units = make(chan gomel.Unit)
+	go func() {
+		config := config.NewDefaultConfiguration()
+		// TODO types
+		ordering := linear.NewOrdering(poset, int(config.VotingLevel), int(config.PiDeltaLevel))
+		level := 0
+		orderedUnits := ordering.TimingRound(level)
+		for orderedUnits != nil {
+			for _, unit := range orderedUnits {
+				units <- unit
+			}
+			level++
+			orderedUnits = ordering.TimingRound(level)
+		}
+		close(units)
+	}()
+	return units
+}
+
+func GetAllTimingUnits(poset gomel.Poset) (units chan gomel.Unit) {
+	units = make(chan gomel.Unit)
+	go func() {
+		config := config.NewDefaultConfiguration()
+		// TODO types
+		ordering := linear.NewOrdering(poset, int(config.VotingLevel), int(config.PiDeltaLevel))
+		level := 0
+		timingUnit := ordering.DecideTimingOnLevel(level)
+		for timingUnit != nil {
+			units <- timingUnit
+			level++
+			timingUnit = ordering.DecideTimingOnLevel(level)
+		}
+		close(units)
+	}()
+	return units
+}
+
+func GetMaximalUnitsSorted(poset gomel.Poset) (units chan gomel.Unit) {
+	units = make(chan gomel.Unit)
+	go func() {
+		poset.MaximalUnitsPerProcess().Iterate(func(forks []gomel.Unit) bool {
+			sorted := make([]gomel.Unit, len(forks))
+			copy(sorted, forks)
+			sort.Slice(sorted, func(i, j int) bool {
+				a, b := sorted[i], sorted[j]
+				return a.Hash().LessThan(b.Hash())
+			})
+			for _, unit := range sorted {
+				units <- unit
+			}
+			return true
+		})
+		close(units)
+	}()
+	return units
+}
+
+func ComposeVerifiers(verifiers ...PosetVerifier) PosetVerifier {
+	return func(posets []*growing.Poset) error {
+		for _, verifier := range verifiers {
+			if err := verifier(posets); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+func VerifyTimingUnits() PosetVerifier {
+	prevLevel := -1
+	return VerifyUnitsUsingOrdering(
+
+		GetAllTimingUnits,
+
+		func(u1, u2 gomel.Unit) error {
+			level := u1.Level()
+			if level != prevLevel+1 {
+				// TODO
+				return gomel.NewDataError(
+					fmt.Sprintf("Missing timing unit for level %d - obtained %d. Unit: %+v", prevLevel+1, level, u1),
+				)
+			}
+			prevLevel = level
+
+			if *u1.Hash() != *u2.Hash() {
+				return gomel.NewDataError("Posets selected different timing units")
+			}
+			return nil
+		},
+	)
+}
+
+func VerifyUnitsUsingOrdering(ordering func(gomel.Poset) chan gomel.Unit, checker func(u1, u2 gomel.Unit) error) PosetVerifier {
+	return func(posets []*growing.Poset) error {
+		if len(posets) < 2 {
+			return nil
+		}
+		for ix := range posets[:len(posets)-1] {
+			fmt.Println("verifying poset no", ix)
+			units1 := ordering(posets[ix])
+			units2 := ordering(posets[ix+1])
+
+			for unit1 := range units1 {
+				unit2, open := <-units2
+
+				if !open {
+					return gomel.NewDataError("Posets contains different number of units")
+				}
+
+				if err := checker(unit1, unit2); err != nil {
+					return err
+				}
+			}
+
+			if _, open := <-units2; open {
+				return gomel.NewDataError("Posets returned different number of units")
+			}
+
+		}
+		return nil
+	}
+}
+
+func VerifyOrdering() PosetVerifier {
+	return VerifyUnitsUsingOrdering(
+
+		GetOrderedUnits,
+
+		func(u1, u2 gomel.Unit) error {
+			if *u1.Hash() != *u2.Hash() {
+				return gomel.NewDataError("Posets differ in ordering")
+			}
+			return nil
+		},
+	)
+}
+
+func VerifyAllPosetsContainSameMaximalUnits() PosetVerifier {
+	return VerifyUnitsUsingOrdering(
+		GetMaximalUnitsSorted,
+
+		func(u1, u2 gomel.Unit) error {
+			if *u1.Hash() != *u2.Hash() {
+				return gomel.NewDataError("posets contains different maximal units")
+			}
+			return nil
+		},
+	)
+}
+
+func NewDefaultVerifier() PosetVerifier {
+	return ComposeVerifiers(VerifyAllPosetsContainSameMaximalUnits(), VerifyTimingUnits(), VerifyOrdering())
+}
+
+func NewNoOpVerifier() PosetVerifier {
+	return func([]*growing.Poset) error {
+		fmt.Println("No verification step")
+		return nil
+	}
+}
+
+func Test(
+	pubKeys []gomel.PublicKey,
+	nUnits, maxParents int,
+	UnitCreator UnitCreator,
+	AddingHandler AddingHandler,
+	verifier PosetVerifier,
+) error {
+
+	nProcesses := len(pubKeys)
+	posets := make([]*growing.Poset, 0, nProcesses)
+
+	for len(posets) < nProcesses {
+		posets = append(posets, growing.NewPoset(&gomel.PosetConfig{Keys: pubKeys}))
+	}
+
+	fmt.Println("Starting a testing routine")
+	for u := 0; u < nUnits; u++ {
+
+		var newUnit gomel.Preunit
+		var err error
+		if newUnit, err = UnitCreator(posets); err != nil {
+			fmt.Fprintln(os.Stderr, "Unable to create a new unit")
+			return err
+		}
+
+		// send the unit to all posets
+		if err := AddingHandler(posets, newUnit); err != nil {
+			fmt.Fprintln(os.Stderr, "Error while adding a unit to some poset:", err)
+			return err
+		}
+	}
+	fmt.Println("Testing routine finished")
+
+	fmt.Println("Verification step")
+	err := verifier(posets)
+	if err != nil {
+		fmt.Println("Posets verfication failed", err.Error())
+		return err
+	}
+	fmt.Println("Verification step finished")
+	return nil
+}
