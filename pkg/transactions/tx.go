6bf797fe77e381d73af40d235538f14ac594ca3a
Michal Handzlik <michal.handzlik@alephzero.org>
2019-06-13 12:23:51 +0200

    spaces -> tabs =='

--- pkg/transactions/tx.go
+++ pkg/transactions/tx.go
@@ -1,65 +1,65 @@
 package transactions
 
 import (
-    "bytes"
-    "compress/gzip"
-    "encoding/binary"
-    "io/ioutil"
+	"bytes"
+	"compress/gzip"
+	"encoding/binary"
+	"io/ioutil"
 )
 
 // Tx is a minimalistic struct for transactions
 type Tx struct {
-    ID       uint32
-    Issuer   string
-    Receiver string
-    Amount   uint32
+	ID       uint32
+	Issuer   string
+	Receiver string
+	Amount   uint32
 }
 
 // Decode decodes bytes to transactions
 func Decode(data []byte) ([]Tx, error) {
-    reader := bytes.NewReader(data)
-    result := []Tx{}
+	reader := bytes.NewReader(data)
+	result := []Tx{}
 
-    var id, amount uint32
-    var issuerLen, receiverLen uint8
-    for reader.Len() > 0 {
-        err := binary.Read(reader, binary.LittleEndian, &id)
-        if err != nil {
-            return result, err
-        }
+	var id, amount uint32
+	var issuerLen, receiverLen uint8
+	for reader.Len() > 0 {
+		err := binary.Read(reader, binary.LittleEndian, &id)
+		if err != nil {
+			return result, err
+		}
 
-        err = binary.Read(reader, binary.LittleEndian, &issuerLen)
-        if err != nil {
-            return result, err
-        }
-        issuer := make([]byte, int(issuerLen))
-        _, err = reader.Read(issuer)
-        if err != nil {
-            return result, err
-        }
+		err = binary.Read(reader, binary.LittleEndian, &issuerLen)
+		if err != nil {
+			return result, err
+		}
+		issuer := make([]byte, int(issuerLen))
+		_, err = reader.Read(issuer)
+		if err != nil {
+			return result, err
+		}
 
-        err = binary.Read(reader, binary.LittleEndian, &receiverLen)
-        if err != nil {
-            return result, err
-        }
-        receiver := make([]byte, int(receiverLen))
-        _, err = reader.Read(receiver)
-        if err != nil {
-            return result, err
-        }
+		err = binary.Read(reader, binary.LittleEndian, &receiverLen)
+		if err != nil {
+			return result, err
+		}
+		receiver := make([]byte, int(receiverLen))
+		_, err = reader.Read(receiver)
+		if err != nil {
+			return result, err
+		}
 
-        err = binary.Read(reader, binary.LittleEndian, &amount)
-        if err != nil {
-            return result, err
-        }
-        result = append(result, Tx{
-            ID:       id,
-            Issuer:   string(issuer),
-            Receiver: string(receiver),
-            Amount:   amount,
-        })
-    }
-    return result, nil
+		err = binary.Read(reader, binary.LittleEndian, &amount)
+		if err != nil {
+			return result, err
+		}
+		result = append(result, Tx{
+			ID:       id,
+			Issuer:   string(issuer),
+			Receiver: string(receiver),
+			Amount:   amount,
+		})
+	}
+	return result, nil
 }
 
 // Encode returns compressed byte representation of given list of transactions.
@@ -71,50 +71,50 @@ func Decode(data []byte) ([]Tx, error) {
 // (5) Receiver
 // (6) Amount as uint32(4 bytes)
 func Encode(txs []Tx) []byte {
-    var data bytes.Buffer
-    for _, tx := range txs {
-        binary.Write(&data, binary.LittleEndian, tx.ID)
-        binary.Write(&data, binary.LittleEndian, uint8(len(tx.Issuer)))
-        data.Write([]byte(tx.Issuer))
-        binary.Write(&data, binary.LittleEndian, uint8(len(tx.Receiver)))
-        data.Write([]byte(tx.Receiver))
-        binary.Write(&data, binary.LittleEndian, tx.Amount)
-    }
-    return data.Bytes()
+	var data bytes.Buffer
+	for _, tx := range txs {
+		binary.Write(&data, binary.LittleEndian, tx.ID)
+		binary.Write(&data, binary.LittleEndian, uint8(len(tx.Issuer)))
+		data.Write([]byte(tx.Issuer))
+		binary.Write(&data, binary.LittleEndian, uint8(len(tx.Receiver)))
+		data.Write([]byte(tx.Receiver))
+		binary.Write(&data, binary.LittleEndian, tx.Amount)
+	}
+	return data.Bytes()
 }
 
 //Compress is compressing the data using gzip on a given level
 func Compress(data []byte, level int) ([]byte, error) {
-    var b bytes.Buffer
-    gz, err := gzip.NewWriterLevel(&b, level)
-    if err != nil {
-        return nil, err
-    }
-    _, err = gz.Write(data)
-    if err != nil {
-        return nil, err
-    }
-    err = gz.Close()
-    if err != nil {
-        return nil, err
-    }
-    return b.Bytes(), nil
+	var b bytes.Buffer
+	gz, err := gzip.NewWriterLevel(&b, level)
+	if err != nil {
+		return nil, err
+	}
+	_, err = gz.Write(data)
+	if err != nil {
+		return nil, err
+	}
+	err = gz.Close()
+	if err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
 }
 
 // Decompress is decompressing given gzipped data
 func Decompress(data []byte) ([]byte, error) {
-    var result []byte
-    gz, err := gzip.NewReader(bytes.NewBuffer(data))
-    if err != nil {
-        return nil, err
-    }
-    result, err = ioutil.ReadAll(gz)
-    if err != nil {
-        return nil, err
-    }
-    err = gz.Close()
-    if err != nil {
-        return nil, err
-    }
-    return result, nil
+	var result []byte
+	gz, err := gzip.NewReader(bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+	result, err = ioutil.ReadAll(gz)
+	if err != nil {
+		return nil, err
+	}
+	err = gz.Close()
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
