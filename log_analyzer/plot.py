5ec04fc3e57b9de6c20cebf3a40ff49863bc6773
Michal Handzlik <michal.handzlik@alephzero.org>
2019-07-01 17:54:40 +0200

    Make plot pipeline work without -a flag

--- log_analyzer/plot.py
+++ log_analyzer/plot.py
@@ -2,6 +2,8 @@ import numpy as np
 regions = np.array(range(128)).reshape(8,-1)
 region_names = ['Virginia','California','Oregon','Ireland','Sao Paulo','Singapore','Sydney','Tokyo']
 
+args.all = True
+
 driver.add_pipeline('', [
     Filter(Event, [SyncStarted, SyncCompleted, DuplicatedUnit]),
     SyncPlots(regions, region_names),
