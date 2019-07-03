627e5ee4310073fa77ab4d278e15190338ff2de5
fruwajacybyk <maciej.gawron@alephzero.co>
2019-07-03 15:53:41 +0200

    more tests

--- pkg/random/tcoin_test.go
+++ pkg/random/tcoin_test.go
@@ -10,14 +10,16 @@ import (
 
 var _ = Describe("Tcoin", func() {
 	var (
+		pid   int
 		poset gomel.Poset
 		rs    gomel.RandomSource
 		err   error
 	)
 	BeforeEach(func() {
+		pid = 0
 		poset, err = tests.CreatePosetFromTestFile("../testdata/empty.txt", tests.NewTestPosetFactory())
 		Expect(err).NotTo(HaveOccurred())
-		rs = NewTcSource(poset, 0)
+		rs = NewTcSource(poset, pid)
 	})
 	Describe("GetCRP", func() {
 		Context("On a given level", func() {
@@ -32,4 +34,25 @@ var _ = Describe("Tcoin", func() {
 			})
 		})
 	})
+	Describe("Update", func() {
+		Context("When adding a dealing unit", func() {
+			Context("without a tc", func() {
+				It("Should return an error", func() {
+					pu := tests.NewPreunit(pid, nil, nil, []byte{0})
+					err := rs.Update(pu)
+					Expect(err).To(HaveOccurred())
+				})
+			})
+			Context("with a tc", func() {
+				It("Should update without errors", func() {
+					rsData := rs.DataToInclude(0, nil, 0)
+					Expect(len(rsData)).NotTo(Equal(0))
+					pu := tests.NewPreunit(pid, nil, nil, rsData)
+					err := rs.Update(pu)
+					Expect(err).NotTo(HaveOccurred())
+				})
+			})
+		})
+	})
+	// TODO: More tests
 })
