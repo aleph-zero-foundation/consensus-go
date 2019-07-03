a8caa6f7b203eb747755ff35b716385002f93813
Michal Handzlik <michal.handzlik@alephzero.org>
2019-07-03 12:02:14 +0200

    Add worker pool to sync package

---
+++ pkg/sync/pool.go
@@ -0,0 +1,40 @@
+package sync
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type pool struct {
+	size int
+	work func()
+	wg   sync.WaitGroup
+	quit int32
+}
+
+func newPool(size int, work func()) *pool {
+	return &pool{
+		size: size,
+		work: work,
+	}
+}
+
+func (p *pool) start() {
+	p.wg.Add(p.size)
+	for i := 0; i < p.size; i++ {
+		go func() {
+			defer p.wg.Done()
+			for {
+				if atomic.LoadInt32(&p.quit) > 0 {
+					return
+				}
+				p.work()
+			}
+		}()
+	}
+}
+
+func (p *pool) stop() {
+	atomic.StoreInt32(&p.quit, 1)
+	p.wg.Wait()
+}
