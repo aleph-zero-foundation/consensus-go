ae72f607f19097a137338f02e1301bf1e016d20d
timorl <tymorl@gmail.com>
2019-07-10 09:51:39 +0200

    No more todos in code

--- .gitlab/ci/make_lint.sh
+++ .gitlab/ci/make_lint.sh
@@ -8,4 +8,17 @@ LINTER_OUTPUT=$2
 PKG_LIST=$(go list ${PKG}/... | grep -v /vendor/)
 
 golint -set_exit_status ${PKG_LIST} | tee ${LINTER_OUTPUT}
-exit ${PIPESTATUS[0]}
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+ exit ${PIPESTATUS[0]}
+fi
+git grep -i "todo" -- :^.gitlab/ci/make_lint.sh | tee -a ${LINTER_OUTPUT}
+stat=${PIPESTATUS[0]}
+if [ $stat -eq 0 ]; then
+	echo "There should not be any TODOs in the code."
+	echo "Remove them and add proper tasks to Jira instead."
+ exit 1
+fi
+if [ $stat -eq 1 ]; then
+	exit 0
+fi
+exit $stat
