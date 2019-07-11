6275bf6b3d3b7053d069de434d582a1911ad3e61
fruwajacybyk <maciej.gawron@alephzero.co>
2019-07-11 16:31:24 +0200

    linter suggestions

--- pkg/random/sync_map.go
+++ pkg/random/sync_map.go
@@ -7,21 +7,26 @@ import (
 	"gitlab.com/alephledger/consensus-go/pkg/crypto/tcoin"
 )
 
+// SyncCSMap is a threadsafe implementation of the map
+// unit's hash => coinShare included in the unit.
 type SyncCSMap struct {
 	sync.RWMutex
 	contents map[gomel.Hash]*tcoin.CoinShare
 }
 
+// NewSyncCSMap returns new SyncCSMap object
 func NewSyncCSMap() *SyncCSMap {
 	return &SyncCSMap{contents: make(map[gomel.Hash]*tcoin.CoinShare)}
 }
 
+// Add adds a coinshare to the map
 func (sm *SyncCSMap) Add(h *gomel.Hash, elem *tcoin.CoinShare) {
 	sm.Lock()
 	defer sm.Unlock()
 	sm.contents[*h] = elem
 }
 
+// Get returns a coinshare saved in the unit having given hash
 func (sm *SyncCSMap) Get(h *gomel.Hash) *tcoin.CoinShare {
 	sm.RLock()
 	defer sm.RUnlock()
