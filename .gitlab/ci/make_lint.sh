8fac97cfa19035eacd48104ebeecd401e3874a34
timorl <tymorl@gmail.com>
2019-07-17 11:36:06 +0200

    Lint fail

--- .gitlab/ci/make_lint.sh
+++ .gitlab/ci/make_lint.sh
@@ -8,8 +8,9 @@ LINTER_OUTPUT=$2
 PKG_LIST=$(go list ${PKG}/... | grep -v /vendor/)
 
 golint -set_exit_status ${PKG_LIST} | tee ${LINTER_OUTPUT}
-if [ ${PIPESTATUS[0]} -ne 0 ]; then
- exit ${PIPESTATUS[0]}
+stat=${PIPESTATUS[0]}
+if [ $stat -ne 0 ]; then
+ exit $stat
 fi
 git grep -i "todo" -- :^.gitlab/ci/make_lint.sh | tee -a ${LINTER_OUTPUT}
 stat=${PIPESTATUS[0]}
