eb1d067c20c24d025c64cae674987b30ded21b4f
Michael Swietek <michael.swietek@alephzero.org>
2019-07-30 15:47:44 +0200

    local experiments with all reasonable sets of sync servers

--- experiments/local/run.sh
+++ experiments/local/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-go run ../../cmd/gomel-keys 4 $1_addrs
+go run ../../cmd/gomel-keys 4 addrs/$1.addrs
 
 for PID in {0..3}
 do
-    go run ../../cmd/gomel --pk $PID.pk --keys_addrs keys_addrs --log $PID.log --config "confs/$1.json"&
+    go run ../../cmd/gomel --pk $PID.pk --keys_addrs keys_addrs --log $PID.$1.log --config "confs/$1.json"&
 done
 
