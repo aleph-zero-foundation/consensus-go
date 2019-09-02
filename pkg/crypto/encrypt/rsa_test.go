2d54293040d20cc193f65e67def16397e8ad9dad
Michael Swietek <michael.swietek@alephzero.org>
2019-09-02 10:25:51 +0200

    tests for encoding

--- pkg/crypto/encrypt/rsa_test.go
+++ pkg/crypto/encrypt/rsa_test.go
@@ -1,7 +1,7 @@
-package encrypto_test
+package encrypt_test
 
 import (
-    "binary"
+    "encoding/binary"
     "bytes"
     "math/rand"
 
@@ -10,7 +10,6 @@ import (
 
 	. "gitlab.com/alephledger/consensus-go/pkg/crypto/encrypt"
 	"gitlab.com/alephledger/consensus-go/pkg/gomel"
-	tests "gitlab.com/alephledger/consensus-go/pkg/tests"
 )
 
 var _ = Describe("Encryption", func() {
@@ -20,7 +19,6 @@ var _ = Describe("Encryption", func() {
 		dk gomel.DecryptionKey
 		ct gomel.CipherText
         err error
-        msg []byte
 	)
 
 	Describe("small", func() {
@@ -30,28 +28,49 @@ var _ = Describe("Encryption", func() {
 		})
 
 		Describe("Checking enc/dec", func() {
+            
+            var msg []byte
 
 			BeforeEach(func() {
-				msg = binary.LittleEndian.Int64(rand.Int())
-                ct, eerr = ek.Encrypt(msg)
+                msg = make([]byte, 8)
+				binary.LittleEndian.PutUint64(msg, rand.Uint64())
+                ct, err = ek.Encrypt(msg)
 			})
 
 			It("Should decrypt correctly", func() {
                 Expect(err).To(BeNil())
                 dmsg, err := dk.Decrypt(ct)
-                Expect(derr).To(BeNil())
-				Expect(bytes.Equal(msg, dmsg).To(BeTrue())
+                Expect(err).To(BeNil())
+				Expect(bytes.Equal(msg, dmsg)).To(BeTrue())
 			})
 
 			It("Should return false for forged ciphertext", func() {
 				ct[0]++
                 dmsg, err := dk.Decrypt(ct)
-                Expect(derr).NotTo(BeNil())
-				Expect(bytes.Equal(msg, dmsg).To(BeFalse())
+                Expect(err).NotTo(BeNil())
+				Expect(bytes.Equal(msg, dmsg)).To(BeFalse())
 			})
 		})
 		Describe("Checking encoding", func() {
+            var text string
+			BeforeEach(func() {
+                text = ek.Encode()
+            })
+
+            It("Should decode correctly", func() {
+                ekd, err := NewEncryptionKey(text)
+                Expect(err).To(BeNil())
+                Expect(eq(ek, ekd)).To(BeTrue())
+            })
+            It("Should throw an error for malformed data", func() {
+                text = "x" + text[1:]
+                _, err := NewEncryptionKey(text)
+                Expect(err).NotTo(BeNil())
+            })
 		})
 	})
-
 })
+
+func eq(ek1, ek2 gomel.EncryptionKey) bool {
+    return ek1.Encode() == ek2.Encode()
+}
