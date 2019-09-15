8a3e58268984dd4df3e69743c3b45a887e43bc7f
Michal Handzlik <michal.handzlik@alephzero.org>
2019-09-15 20:21:43 +0200

    New definitions for Server interface

--- pkg/sync/server.go
+++ pkg/sync/server.go
@@ -1,6 +1,8 @@
 // Package sync defines the primitives needed for various synchronization algorithms.
 package sync
 
+import "gitlab.com/alephledger/consensus-go/pkg/gomel"
+
 // Server is responsible for handling a sync protocol.
 type Server interface {
 	// Start the server.
@@ -9,4 +11,16 @@ type Server interface {
 	StopIn()
 	// StopOut stops handling outgoing synchronizations.
 	StopOut()
+}
+
+// QueryServer is a Server that can find out information about an unknown preunit.
+type QueryServer interface {
+	Server
+	FindOut(gomel.Preunit)
+}
+
+// MulticastServer is a Server that can multicast units to other committee members.
+type MulticastServer interface {
+	Server
+	Send(gomel.Unit)
 }
