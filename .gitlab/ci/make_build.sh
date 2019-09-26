673c870f8713cf42d8cd577251c61b00e6ab41e3
Łukasz Lachowski <lukasz.lachowski@alephzero.org>
2019-09-26 19:18:01 +0200

    Added "set -e" in all CI scripts to fail as soon as some command fail.

--- .gitlab/ci/make_build.sh
+++ .gitlab/ci/make_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
 PKG=$1
 
-go build ${PKG}/... && \
-    find . -iname "*_test.go" -print0 | xargs -0 -n1 go test -c
+go build ${PKG}/...
+find . -iname "*_test.go" -print0 | xargs -0 -n1 go test -c
--- .gitlab/ci/make_race.sh
+++ .gitlab/ci/make_race.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 PKG=$1
 
