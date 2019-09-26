3a9a7976315438b42ad3edc899b75037095c34ec
Łukasz Lachowski <lukasz.lachowski@alephzero.org>
2019-09-26 17:55:59 +0200

    Fixed script installing dependencies for CI.

--- .gitlab/ci/make_dep.sh
+++ .gitlab/ci/make_dep.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-go get github.com/onsi/ginkgo/ginkgo
-go get github.com/onsi/gomega/...
-
-go get -u github.com/golang/lint/golint
-
-go get -v -d ./...
+go get github.com/onsi/ginkgo/ginkgo && \
+    go get github.com/onsi/gomega/... && \
+    go get -u golang.org/x/lint/golint && \
+    go get -v -d ./...
