901688241692bff23a35371a66cb39533cb3de13
Michal Handzlik <michal.handzlik@alephzero.org>
2019-10-22 22:37:48 +0200

    get rid of obsolete code

--- pkg/dag/adding.go
+++ pkg/dag/adding.go
@@ -5,65 +5,6 @@ import (
 	"gitlab.com/alephledger/consensus-go/pkg/gomel"
 )
 
-func (dag *dag) Decode(pu gomel.Preunit) (gomel.Unit, error) {
-	if pu.Creator() < 0 || pu.Creator() >= dag.nProcesses {
-		return nil, gomel.NewDataError("invalid creator")
-	}
-	if u := dag.GetUnit(pu.Hash()); u != nil {
-		return nil, gomel.NewDuplicateUnit(u)
-	}
-	possibleParents := dag.heightUnits.get(pu.View().Heights)
-	parents, err := getParents(possibleParents, pu.Creator())
-	if err != nil {
-		return nil, err
-	}
-
-	if unknown := countUnknown(parents, pu.View().Heights); unknown > 0 {
-		return nil, gomel.NewUnknownParents(unknown)
-	}
-
-	if *gomel.CombineHashes(gomel.ToHashes(parents)) != pu.View().ControlHash {
-		return nil, gomel.NewDataError("wrong control hash")
-	}
-
-	return unit.New(pu, parents), nil
-}
-
-func toHashes(units []gomel.Unit) []*gomel.Hash {
-	result := make([]*gomel.Hash, len(units))
-	for i, u := range units {
-		if u != nil {
-			result[i] = u.Hash()
-		}
-	}
-	return result
-}
-
-func countUnknown(parents []gomel.Unit, heights []int) int {
-	unknown := 0
-	for i, h := range heights {
-		if h != -1 && parents[i] == nil {
-			unknown++
-		}
-	}
-	return unknown
-}
-
-func getParents(units [][]gomel.Unit, pid uint16) ([]gomel.Unit, error) {
-	nProc := len(units)
-	result := make([]gomel.Unit, nProc)
-
-	for i, us := range units {
-		if us != nil {
-			result[i] = us[0]
-		}
-		if len(us) > 1 {
-			return nil, gomel.NewAmbiguousParents(units)
-		}
-	}
-	return result, nil
-}
-
 func (dag *dag) Prepare(u gomel.Unit) (gomel.Unit, error) {
 	return unit.Prepared(u, dag), nil
 }
