5f02065f4c770e56e46a7a3df971f553b388bf64
Michal Handzlik <michal.handzlik@alephzero.org>
2019-10-31 10:39:48 +0100

    Pacify byzantine test

--- tests/offline_dag/byzantine_tests/byzantine_linear_ordering_test.go
+++ tests/offline_dag/byzantine_tests/byzantine_linear_ordering_test.go
@@ -607,7 +607,7 @@ func syncDags(dag1, dag2 gomel.Dag, rs1, rs2 gomel.RandomSource) (bool, error) {
 				// descend to a common parent
 				current := unit
 				for current != nil {
-					other, _ := dag.GetUnits([]*gomel.Hash{current.Hash()})
+					other := dag.GetUnits([]*gomel.Hash{current.Hash()})
 					if len(other) < 1 || other[0] == nil {
 						if missing[current] {
 							break
