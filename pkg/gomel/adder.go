6484891c903dd36af137d3026ce7b738dae2eca8
Michal Handzlik <michal.handzlik@alephzero.org>
2020-02-05 16:22:02 +0100

    typo

--- pkg/gomel/adder.go
+++ pkg/gomel/adder.go
@@ -2,7 +2,7 @@ package gomel
 
 // Adder is a component that accepts incoming preunits.
 type Adder interface {
-	// AddPrenits adds preunits received from the given process.
+	// AddPreunits adds preunits received from the given process.
 	AddPreunits(uint16, ...Preunit) []error
 	// Close stops the Adder.
 	Close()
