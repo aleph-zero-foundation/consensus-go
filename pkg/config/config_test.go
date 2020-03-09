81eab9b3a9897007d3e96256903518aca14b9593
Michal Swietek <michael.swietek@alephzero.org>
2020-03-09 14:38:20 +0100

    adds tests for config validity

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -14,6 +14,40 @@ import (
 )
 
 var _ = Describe("Config", func() {
+	Describe("committee member", func() {
+		Describe("When loaded from a test file ", func() {
+			It("Should work without errors", func() {
+				file, err := os.Open("../testdata/test_pk.txt")
+				defer file.Close()
+				Expect(err).NotTo(HaveOccurred())
+				reader := bufio.NewReader(file)
+				_, err = LoadMember(reader)
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+		Describe("When loaded from a test file and stored in a buffer", func() {
+			var member *Member
+			var fileContent []byte
+			BeforeEach(func() {
+				file, err := os.Open("../testdata/test_pk.txt")
+				defer file.Close()
+				Expect(err).NotTo(HaveOccurred())
+
+				reader := bufio.NewReader(file)
+				member, err = LoadMember(reader)
+				Expect(err).NotTo(HaveOccurred())
+
+				fileContent, err = ioutil.ReadFile("../testdata/test_pk.txt")
+				Expect(err).NotTo(HaveOccurred())
+			})
+			It("Should have the same content", func() {
+				buf := bytes.NewBuffer([]byte{})
+				err := StoreMember(buf, member)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(buf.Bytes()).To(Equal(fileContent))
+			})
+		})
+	})
 	Describe("committee", func() {
 		Describe("When loaded from a test file ", func() {
 			It("Should work without errors", func() {
@@ -48,38 +82,38 @@ var _ = Describe("Config", func() {
 			})
 		})
 	})
-	Describe("committee member", func() {
-		Describe("When loaded from a test file ", func() {
-			It("Should work without errors", func() {
-				file, err := os.Open("../testdata/test_pk.txt")
-				defer file.Close()
-				Expect(err).NotTo(HaveOccurred())
-				reader := bufio.NewReader(file)
-				_, err = LoadMember(reader)
-				Expect(err).NotTo(HaveOccurred())
-			})
-		})
-		Describe("When loaded from a test file and stored in a buffer", func() {
-			var member *Member
-			var fileContent []byte
-			BeforeEach(func() {
-				file, err := os.Open("../testdata/test_pk.txt")
-				defer file.Close()
-				Expect(err).NotTo(HaveOccurred())
+	Describe("defaults", func() {
+		var (
+			m *Member
+			c *Committee
+		)
+		BeforeEach(func() {
+			file, err := os.Open("../testdata/test_pk.txt")
+			defer file.Close()
+			Expect(err).NotTo(HaveOccurred())
+			reader := bufio.NewReader(file)
+			m, err = LoadMember(reader)
+			Expect(err).NotTo(HaveOccurred())
 
-				reader := bufio.NewReader(file)
-				member, err = LoadMember(reader)
-				Expect(err).NotTo(HaveOccurred())
-
-				fileContent, err = ioutil.ReadFile("../testdata/test_pk.txt")
-				Expect(err).NotTo(HaveOccurred())
-			})
-			It("Should have the same content", func() {
-				buf := bytes.NewBuffer([]byte{})
-				err := StoreMember(buf, member)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(buf.Bytes()).To(Equal(fileContent))
-			})
+			file, err = os.Open("../testdata/test_committee.txt")
+			defer file.Close()
+			Expect(err).NotTo(HaveOccurred())
+			reader = bufio.NewReader(file)
+			c, err = LoadCommittee(reader)
+			Expect(err).NotTo(HaveOccurred())
 		})
+		It("should generate valid setup config", func() {
+			cnf, err := NewSetup(m, c)
+			Expect(err).NotTo(HaveOccurred())
+			err = Valid(cnf, true)
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("should generate valid consensus config", func() {
+			cnf, err := New(m, c)
+			Expect(err).NotTo(HaveOccurred())
+			err = Valid(cnf, false)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
 	})
 })
