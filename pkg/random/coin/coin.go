8d3824bcc80baca0be30111f892835e6d02aee4c
Michal Handzlik <michal.handzlik@alephzero.org>
2020-05-19 14:15:16 +0200

    Change confusing check in DataToInclude

--- pkg/random/coin/coin.go
+++ pkg/random/coin/coin.go
@@ -152,11 +152,10 @@ func (c *coin) checkCompliance(u gomel.Unit, _ gomel.Dag) error {
 // it returns an error. This means that someone had included a wrong coin share
 // and we should start an alert.
 func (c *coin) DataToInclude(parents []gomel.Unit, level int) ([]byte, error) {
-	var rb []byte
-	// only share providers are expected to provide data related with random-source in their dealing units (see coin.checkCompliance)
-	if level == 0 && !c.shareProviders[c.pid] {
-		return rb, nil
+	if level == 0 {
+		panic("coin.DataToInclude called on a dealing unit")
 	}
+	var rb []byte
 	if rbl := c.randomBytes.Get(level - 1); rbl != nil {
 		rb = make([]byte, bn256.SignatureLength)
 		copy(rb, rbl)
